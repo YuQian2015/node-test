9695ca0e348a08d62c5ef6308517943350f268e2
yuqian <757275030@qq.com>
2017-07-13 16:26:01 +0800

    修改上传

--- server/server.js
+++ server/server.js
@@ -92,11 +92,11 @@ mongo.connect(function (db) {
   //   res.json({result: 'success', data: req.body});
   // });
 
-  app.post('/uploadFile', function (req, res) {
+  app.post('/api/uploadFile', function (req, res) {
 
    console.log(req.files[0]);  // 上传的文件信息
 
-   var des_file = __dirname + "/" + req.files[0].originalname;
+   var des_file = __dirname + "/upload/" + req.files[0].originalname;
    fs.readFile( req.files[0].path, function (err, data) {
         fs.writeFile(des_file, data, function (err) {
          if( err ){
