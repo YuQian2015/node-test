a4fe5427237184661e729e5df8df9d420f2d3e8c
yuqian <757275030@qq.com>
2017-07-18 19:00:11 +0800

    token

---
+++ server/config/config.js
@@ -0,0 +1,11 @@
+var appConfig = {
+  mongoDB:"mongodb://localhost:3001/tjjy",
+
+
+  // api
+  register:"/api/register",
+
+  uploadFile:"/api/uploadFile"
+}
+
+module.exports = appConfig;
---
+++ server/index.js
@@ -0,0 +1,42 @@
+let express = require('express');
+let app = express();
+let bodyParser  = require('body-parser');
+let mongoose    = require('mongoose');
+global.config = require('./config/config');
+
+let jwt    = require('jsonwebtoken');
+let User   = require('./models/user');
+
+mongoose.connect(global.config.mongoDB);
+
+/**
+  * 连接成功
+  */
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connection open to ' + global.config.mongoDB);
+});
+
+/**
+ * 连接异常
+ */
+mongoose.connection.on('error',function (err) {
+    console.log('Mongoose connection error: ' + err);
+});
+
+/**
+ * 连接断开
+ */
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose connection disconnected');
+});
+
+
+
+
+app.use(bodyParser.json());
+
+app.use(require('./controllers'));
+
+app.listen(3000, function(){
+    console.log('App running on 3000');
+});
