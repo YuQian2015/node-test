156b02f14c87bf758167908469c5b4fdaec1b4bd
yuqian <757275030@qq.com>
2017-08-10 14:35:53 +0800

    server

--- server/config/config.js
+++ server/config/config.js
@@ -11,15 +11,28 @@ var appConfig = {
   // UPLOAD_IMG_THUMB_DIR:"F:/project/tjjy/static/images/thumb/",//图片预览地址
   // FROALA_UPLOAD_IMG_DIR:"F:/project/tjjy/static/images/froala/",//编辑器图片上传地址
   // FROALA_UPLOAD_IMG_THUMB_DIR:"F:/project/tjjy/static/images/froala/thumb/",//编辑器图片预览地址
-  UPLOAD_IMG_DIR:"D:/nginx/tjjy/static/images/",
-  UPLOAD_IMG_THUMB_DIR:"D:/nginx/tjjy/static/images/thumb/",
-  FROALA_UPLOAD_IMG_DIR:"D:/nginx/tjjy/static/images/froala/",
-  FROALA_UPLOAD_IMG_THUMB_DIR:"D:/nginx/tjjy/static/images/froala/thumb/",
+
+  UPLOAD_IMG_DIR:"C:/tjjy/static/images/",
+  UPLOAD_IMG_THUMB_DIR:"C:/tjjy/static/images/thumb/",
+  FROALA_UPLOAD_IMG_DIR:"C:/tjjy/static/images/froala/",
+  FROALA_UPLOAD_IMG_THUMB_DIR:"C:/tjjy/static/images/froala/thumb/",
+
+  // UPLOAD_IMG_DIR:"D:/nginx/tjjy/static/images/",
+  // UPLOAD_IMG_THUMB_DIR:"D:/nginx/tjjy/static/images/thumb/",
+  // FROALA_UPLOAD_IMG_DIR:"D:/nginx/tjjy/static/images/froala/",
+  // FROALA_UPLOAD_IMG_THUMB_DIR:"D:/nginx/tjjy/static/images/froala/thumb/",
+
+  // IMG_URL:"http://localhost:8089/static/images/",
+  // IMG_THUMB_URL:"http://localhost:8089/static/images/thumb/",
+  // FROALA_IMG_URL:"http://localhost:8089/static/images/froala/",
+  // FROALA_IMG_THUMB_URL:"http://localhost:8089/static/images/froala/thumb/"
   
-  IMG_URL:"http://localhost:8089/static/images/",
-  IMG_THUMB_URL:"http://localhost:8089/static/images/thumb/",
-  FROALA_IMG_URL:"http://localhost:8089/static/images/froala/",
-  FROALA_IMG_THUMB_URL:"http://localhost:8089/static/images/froala/thumb/"
+  IMG_URL:"http://47.92.101.205:8089/static/images/",
+  IMG_THUMB_URL:"http://47.92.101.205:8089/static/images/thumb/",
+  FROALA_IMG_URL:"http://47.92.101.205:8089/static/images/froala/",
+  FROALA_IMG_THUMB_URL:"http://47.92.101.205:8089/static/images/froala/thumb/"
+
+
 
 }
 
