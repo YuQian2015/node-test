5ec97c2f20ccf0c1d0ca03a42987f2c0869f570b
Yu Qian <yuqian.124@gamil.com.com>
2017-12-17 11:22:02 +0800

    修改文件目录

--- server/config/config.js
+++ server/config/config.js
@@ -7,27 +7,30 @@ var appConfig = {
   mongoDB:"mongodb://localhost:3001/tjjy",
   jwt_secret:"tjjy",
 
+  // 公司电脑
+  // UPLOAD_IMG_DIR:"F:/project/tjjy/static/images/",//图片上传地址
+  // UPLOAD_IMG_THUMB_DIR:"F:/project/tjjy/static/images/thumb/",//图片预览地址
+  // FROALA_UPLOAD_IMG_DIR:"F:/project/tjjy/static/images/froala/",//编辑器图片上传地址
+  // FROALA_UPLOAD_IMG_THUMB_DIR:"F:/project/tjjy/static/images/froala/thumb/",//编辑器图片预览地址
 
-  UPLOAD_IMG_DIR:"F:/project/tjjy/static/images/",//图片上传地址
-  UPLOAD_IMG_THUMB_DIR:"F:/project/tjjy/static/images/thumb/",//图片预览地址
-  FROALA_UPLOAD_IMG_DIR:"F:/project/tjjy/static/images/froala/",//编辑器图片上传地址
-  FROALA_UPLOAD_IMG_THUMB_DIR:"F:/project/tjjy/static/images/froala/thumb/",//编辑器图片预览地址
-
+  // 服务器
   // UPLOAD_IMG_DIR:"C:/tjjy/static/images/",
   // UPLOAD_IMG_THUMB_DIR:"C:/tjjy/static/images/thumb/",
   // FROALA_UPLOAD_IMG_DIR:"C:/tjjy/static/images/froala/",
   // FROALA_UPLOAD_IMG_THUMB_DIR:"C:/tjjy/static/images/froala/thumb/",
 
-  // UPLOAD_IMG_DIR:"D:/nginx/tjjy/static/images/",
-  // UPLOAD_IMG_THUMB_DIR:"D:/nginx/tjjy/static/images/thumb/",
-  // FROALA_UPLOAD_IMG_DIR:"D:/nginx/tjjy/static/images/froala/",
-  // FROALA_UPLOAD_IMG_THUMB_DIR:"D:/nginx/tjjy/static/images/froala/thumb/",
+  // 自己电脑
+  UPLOAD_IMG_DIR:"D:/nginx/tjjy/static/images/",
+  UPLOAD_IMG_THUMB_DIR:"D:/nginx/tjjy/static/images/thumb/",
+  FROALA_UPLOAD_IMG_DIR:"D:/nginx/tjjy/static/images/froala/",
+  FROALA_UPLOAD_IMG_THUMB_DIR:"D:/nginx/tjjy/static/images/froala/thumb/",
 
   IMG_URL:"http://localhost:8089/static/images/",
   IMG_THUMB_URL:"http://localhost:8089/static/images/thumb/",
   FROALA_IMG_URL:"http://localhost:8089/static/images/froala/",
   FROALA_IMG_THUMB_URL:"http://localhost:8089/static/images/froala/thumb/"
 
+  // 服务器
   // IMG_URL:"http://47.92.101.205:8089/static/images/",
   // IMG_THUMB_URL:"http://47.92.101.205:8089/static/images/thumb/",
   // FROALA_IMG_URL:"http://47.92.101.205:8089/static/images/froala/",
